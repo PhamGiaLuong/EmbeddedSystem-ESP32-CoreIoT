cff6dd762ae8881b96e22f5564d45fe9725a0acf
PhamGiaLuong <pham15032004@gmail.com>
2025-10-05 16:33:55 +0700

    update ArduinoJson library

--- lib/ArduinoJson/ArduinoJson.h
+++ lib/ArduinoJson/ArduinoJson.h
@@ -1,17 +1,5 @@
 // ArduinoJson - https://arduinojson.org
-// Copyright © 2014-2025, Benoit BLANCHON
+// Copyright © 2014-2023, Benoit BLANCHON
 // MIT License
 
-#pragma once
-
-#ifdef __cplusplus
-
-#  include "ArduinoJson.hpp"
-
-using namespace ArduinoJson;
-
-#else
-
-#error ArduinoJson requires a C++ compiler, please change file extension to .cc or .cpp
-
-#endif
+#include "src/ArduinoJson.h"

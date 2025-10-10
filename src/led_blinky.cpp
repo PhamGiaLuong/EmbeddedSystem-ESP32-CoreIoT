cea96d38bb2b1b1339bd938a05c2e1d6f375dd90
PhamGiaLuong <pham15032004@gmail.com>
2025-10-10 22:28:29 +0700

    Change leb blinky behavior according to temp data

--- src/led_blinky.cpp
+++ src/led_blinky.cpp
@@ -1,12 +1,27 @@
 #include "led_blinky.h"
 
 void led_blinky(void *pvParameters){
+	TickType_t lastWakeTime = xTaskGetTickCount();
+  	TickType_t period = pdMS_TO_TICKS(NORMAL_PERIOD);
     pinMode(LED_GPIO, OUTPUT);
+    bool ledState = false;
   
-    while(1) {                        
-        digitalWrite(LED_GPIO, HIGH);
-        vTaskDelay(1000);
-        digitalWrite(LED_GPIO, LOW);
-        vTaskDelay(1000);
+    while(1) {       
+        if(xSemaphoreTake(mutexForTempData, portMAX_DELAY) == pdTRUE) {
+            if(glob_temperature == -1) {
+                period = pdMS_TO_TICKS(FAST_PERIOD);
+            } else if(glob_temperature >= 30) {
+                period = pdMS_TO_TICKS(SLOW_PERIOD);
+            } else {
+                period = pdMS_TO_TICKS(NORMAL_PERIOD);
+            }
+            xSemaphoreGive(mutexForTempData);
+        } else {
+            Serial.println(ERR_TEMP_TIMEOUT);
+        }
+        vTaskDelayUntil(&lastWakeTime, period);
+
+        ledState = !ledState;
+        digitalWrite(LED_GPIO, ledState);
     }
 }

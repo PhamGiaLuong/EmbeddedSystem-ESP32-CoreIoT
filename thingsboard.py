df38b69a385021c1071ef780d303c20f5a29b883
PhamGiaLuong <pham15032004@gmail.com>
2025-10-10 21:08:59 +0700

    Vid4: add latitude and longitude params

--- thingsboard.py
+++ thingsboard.py
@@ -50,8 +50,24 @@ temp = 30
 humi = 50
 light_intesity = 100
 counter = 0
+
+#HCMUT
+# long = 106.65789107082472
+# lat = 10.772175109674038
+
+#H6
+# long = 106.80633605864662
+# lat = 10.880018410410052
+
+#MH
+long = 106.829815
+lat = 10.848297
+
+
 while True:
-    collect_data = {'temperature': temp, 'humidity': humi, 'light':light_intesity}
+    collect_data = {'temperature': temp, 'humidity': humi,
+                    'light':light_intesity,
+                    'long': long, 'lat': lat}
     temp += 1
     humi += 1
     light_intesity += 1
